959d63285909966548b14ebae2846cc9a86ad4c5
Loc Nguyen <locnguyenddbrvt.dev@gmail.com>
2023-09-05 07:51:03 +0700

    init template

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
-import { NestFactory } from '@nestjs/core';
+import { NestApplication, NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
+import { join } from 'path';
+import { Request, Response, NextFunction, json, urlencoded } from 'express';
+// import { swaggerConfig } from './config/swagger.config';
 import { AppModule } from './app.module';
+// import { ENV } from './types/enum/env';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestApplication>(AppModule);
+  const configService = app.get(ConfigService);
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    res.removeHeader('x-powered-by');
+    res.removeHeader('date');
+    next();
+  });
+  app.enableCors({
+    origin: configService.get<string>('ENV.ALLOW_ORIGINS').split(','),
+    methods: ['GET', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    credentials: true,
+  });
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ limit: '50mb', extended: true }));
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+  app.setGlobalPrefix('api');
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'src', 'views'));
+  app.setViewEngine('hbs');
+  // swaggerConfig(app);
   await app.listen(3000);
 }
 bootstrap();
