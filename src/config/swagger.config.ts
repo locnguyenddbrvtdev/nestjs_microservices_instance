549c7e706a971905e0811b045724911d325da978
Loc Nguyen <locnguyenddbrvt.dev@gmail.com>
2023-09-05 12:34:44 +0700

    change swagger title to dynamic

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -1,9 +1,13 @@
 import { INestApplication } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppConfigService } from '@modules/config/config.service';
 
 export const swaggerConfig = (app: INestApplication) => {
+  const appConfigService = app.get(AppConfigService);
+  const thisService = appConfigService.thisService();
+
   const config = new DocumentBuilder()
-    .setTitle('.... SERVICES DOCUMENT')
+    .setTitle(`${thisService} SERVICES DOCUMENT`)
     .setDescription('API definition')
     .setVersion('0.1')
     .addBearerAuth()
